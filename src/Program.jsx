2b9e5c0e26682a93ffa67558b7c309aa650fae95
gudgle <108617925+gudgle@users.noreply.github.com>
2022-09-11 19:02:32 +0200

    Update Program.jsx

--- src/Program.jsx
+++ src/Program.jsx
@@ -4,27 +4,47 @@ const Program = () => {
   return (
     <div id="Program">
     <p>
-    Velkommen til hjemmesiden for bryllupet vårt!
-    <br>
-    </br>
-    Her finner du programmet og all informasjon om bryllupshelgen. 
-    <br>
-    </br>
-    Svar utbes innen 01. mai 2023 via skjemaet dere finner under <i> Svar her </i>.
-    </p>
-    <p>
-    
-    - Gudrun & Simon -
-    
-    </p>
-    <h1>
-        Program
-      </h1>
-      <p>
-        Program kommer snart!
-    
-     (Husk link til kirken og huset. Er ikke adresser på det på maps)
+      Velkommen til hjemmesiden for bryllupet vårt!
+      <br>
+      </br>
+      Her finner du programmet og all informasjon om bryllupshelgen. 
+      <br>
+      </br>
+      Svar utbes innen 01. mai 2023 via skjemaet dere finner under <i> Svar her </i>.
       </p>
+      <p> 
+      - Gudrun & Simon -
+     </p>
+    
+    <h1>
+      Program
+    </h1>
+      <div className="bullets">
+        <div>
+          <h2>
+            Fredag
+          </h2>
+          <p>
+            Fredagtekst 
+          </p>
+        </div>
+        <div>
+          <h2>
+            Lørdag
+          </h2>
+          <p>
+            lørdagstekst
+          </p>
+        </div>
+        <div>
+          <h2>
+            SØndag
+          </h2>
+          <p>
+            søndagstekst
+          </p>
+        </div>
+      </div>
     </div>
   );
 };
