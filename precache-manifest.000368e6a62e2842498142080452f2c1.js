3da68d5b1bb820014b740c14c127bbcbd8734aad
gudgle <108617925+gudgle@users.noreply.github.com>
2022-10-01 14:08:06 +0000

    Deploying to gh-pages from @ melhoos/gudrunogsimon@69554639b3c5abb6fd9d9d5c92f931f551e6895f 🚀

---
+++ precache-manifest.000368e6a62e2842498142080452f2c1.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "bb3a3cae4e4cf31f1e21",
+    "url": "/static/css/main.612e2917.chunk.css"
+  },
+  {
+    "revision": "bb3a3cae4e4cf31f1e21",
+    "url": "/static/js/main.bb3a3cae.chunk.js"
+  },
+  {
+    "revision": "709d58a229027795f766",
+    "url": "/static/js/1.709d58a2.chunk.js"
+  },
+  {
+    "revision": "cf1e4ed02574bb4c8b79",
+    "url": "/static/js/2.cf1e4ed0.chunk.js"
+  },
+  {
+    "revision": "6ee34327ee87b8f52b89",
+    "url": "/static/js/runtime~main.6ee34327.js"
+  },
+  {
+    "revision": "2a11a95626a0ea50f683189ad5d62785",
+    "url": "/static/media/heart.2a11a956.png"
+  },
+  {
+    "revision": "a5f09a16f89976086d6c0e6585a8cba6",
+    "url": "/static/media/gudrunogsimon.a5f09a16.jpg"
+  },
+  {
+    "revision": "3bf09aa4a60836db83cd1e3ec0c820dc",
+    "url": "/static/media/gudrunogsimon2.3bf09aa4.jpg"
+  },
+  {
+    "revision": "aa00c5d91c47e7b718b5cdca3dea7875",
+    "url": "/static/media/FiraMono-Regular.aa00c5d9.ttf"
+  },
+  {
+    "revision": "c892df27b0d92f88b72752269511c31d",
+    "url": "/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.10913b91faf5ed20d2692907cf48d1cb.js"
+  "/precache-manifest.000368e6a62e2842498142080452f2c1.js"
 );
 
 workbox.clientsClaim();
