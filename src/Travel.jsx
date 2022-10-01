ca8ac704d2adb70aad4c83c72b889910d7b71b09
gudgle <108617925+gudgle@users.noreply.github.com>
2022-10-01 14:09:06 +0200

    Update Travel.jsx

--- src/Travel.jsx
+++ src/Travel.jsx
@@ -19,13 +19,21 @@ const Travel = () => {
             Hurtigbåt
           </h3>
           <p>
-            Det går hurtigbåt fra Trondheim Hurtigbåtterminal til Sandstad på Hitra. Videre går det korresponderende buss (422 Dyrøy) til Fillan Busstasjon (se atb.no for rutetider). Turen tar 2 til 2,5 time, avhengig av rute. Det er mulig å komme seg videre til Fjellværøya/Ulvøya med buss, men det lureste er å høre med noen med bil som kan plukke deg opp.
+            Det går hurtigbåt fra Trondheim Hurtigbåtterminal til Sandstad på Hitra. Videre går det korresponderende buss (422 Dyrøy) til Fillan Busstasjon (Se {' '}
+            <a
+              href="https://www.atb.no/reiseplanlegger/?direction=1&adv=1&GetTR0=1&GetTR4=1&tplang=no&from=Trondheim%20Hurtigb%C3%A5tterminal%20(Trondheim)&to=Sandstad%20vegstasjon%20(Hitra)&Time=14%3A40&Date=03.10.2022&changepause=0&changepenalty=1"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+            atb.no
+            </a>
+            {' '} for rutetider). Turen tar 2 til 2,5 time, avhengig av rute. Det er mulig å komme seg videre til Fjellværøya/Ulvøya med buss, men det lureste er å høre med noen med bil som kan plukke deg opp.
           </p>
           <h3>
             Direktebuss
           </h3>
           <p>
-            Ta buss 420 mot Frøya via Orkanger og Hitra fra Trondheim Sentralstasjon. Turen tar drøye 2,5 time. Se {' '}
+            Ta buss 420 mot Frøya via Orkanger og Hitra fra Trondheim Sentralstasjon til Fillan Busstasjon. Turen tar drøye 2,5 time. Se {' '}
             <a
               href="https://www.atb.no/reiseplanlegger/?direction=1&adv=1&GetTR0=1&GetTR1=1&tplang=no&from=Trondheim%20Sentralstasjon%20(Trondheim)&to=Fillan%20busstasjon%20(Hitra)&Time=14%3A40&Date=03.10.2022&changepause=0&changepenalty=1"
               target="_blank"
@@ -33,7 +41,7 @@ const Travel = () => {
             >
             atb.no
             </a>
-            for rutetider.
+            {' '} for rutetider.
           </p>
   
           <h3>
