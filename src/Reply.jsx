b84bf4f3ff9548ffb1aea5572aa8563f816a7c9f
gudgle <108617925+gudgle@users.noreply.github.com>
2022-10-02 16:39:58 +0200

    Update Reply.jsx

--- src/Reply.jsx
+++ src/Reply.jsx
@@ -7,12 +7,12 @@ const Reply = () => {
         Svar her
       </h1>
       <p>
-        Svar utbes innen 01. mai 2023 via dette svarskjema.
+        Svar utbes innen 1. mai 2023 via svarskjemaet. Det er mulig å gjøre endringer frem til 1. mai 2023.
       </p>
       <p>
         Vi ønsker at alle svarer, uavhengig om dere kommer eller ei. 
       <br/>
-      Det er mulig å svare for flere personer samtiig, men vi oppfordrer til å svare individuelt. 
+      Det er mulig å svare for flere personer samtidig, men vi oppfordrer til å svare individuelt. 
       </p>
     </div>
   );
