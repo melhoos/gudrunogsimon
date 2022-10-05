47f04a05610372d1dee563c0c92f311c88d48428
gudgle <108617925+gudgle@users.noreply.github.com>
2022-10-05 22:04:45 +0200

    Update Contact.jsx

--- src/Contact.jsx
+++ src/Contact.jsx
@@ -10,7 +10,7 @@ const Contact = () => {
         Er det noe du lurer på? Kontakt Simon (976 11 260, simbrafje@gmail.com) eller Gudrun (994 06 961, gudrunglende@gmail.com).
       </p>
       <p>
-        Ønsker du å si noen ord, fremføre en musikal, lese et dikt eller danse magedans? Kontakt en av toastmasterne; Martin Næss (473 01 493/martin.neess@gmail.com) eller Tobias Korsmoe (995 83 029/tobiaskorsmoe@gmail.com). 
+        Ønsker du å si noen ord, fremføre en musikal, lese et dikt eller danse magedans? Kontakt en av toastmasterne våre; Martin Næss (473 01 493/martin.neess@gmail.com) eller Tobias Korsmoe (995 83 029/tobiaskorsmoe@gmail.com). 
       </p>
     </div>
   );
