6b5e90c80b141775ac17693ebe85c7593d770ca7
gudgle <108617925+gudgle@users.noreply.github.com>
2022-10-05 22:22:20 +0200

    Update Reply.jsx

--- src/Reply.jsx
+++ src/Reply.jsx
@@ -10,10 +10,17 @@ const Reply = () => {
         Svar utbes innen 1. mai 2023 via svarskjemaet. Det er mulig å gjøre endringer frem til 1. mai 2023.
       </p>
       <p>
-        Vi ønsker at alle svarer, uavhengig om dere kommer eller ei. 
-      <br/>
-      Det er mulig å svare for flere personer samtidig, men vi oppfordrer til å svare individuelt. 
+        Vi ønsker at alle svarer, uavhengig om dere kommer eller ei. Det er mulig å svare for flere personer samtidig, men vi oppfordrer til å svare individuelt. 
       </p>
+    <p>
+    <a
+              href="https://forms.gle/sxc3514rrAXLA2vMA"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+            Svar her
+            </a>
+    </p>
     </div>
   );
 };
