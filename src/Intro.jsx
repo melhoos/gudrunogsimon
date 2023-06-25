410b6603d5a6086573cf67f2be9da1f9a937de0a
gudgle <108617925+gudgle@users.noreply.github.com>
2023-06-25 22:17:43 +0200

    Update Intro.jsx

--- src/Intro.jsx
+++ src/Intro.jsx
@@ -7,12 +7,11 @@ const Intro = () => {
             Velkommen til hjemmesiden for bryllupet vårt!
             <br/>
             Her finner du programmet og all informasjon om bryllupshelgen. 
-            <br/>
-            Svar utbes innen 1. mai 2023 via skjemaet dere finner under <i> Svar her</i>.
         </p>
         <p> 
             - Gudrun & Simon -
         </p>
+        <br/>
      </div>
   );
 };
