c2a1cbd4f446329d590a85175ad638a8707d3f52
gudgle <108617925+gudgle@users.noreply.github.com>
2023-06-25 22:58:18 +0200

    Update Program.jsx

--- src/Program.jsx
+++ src/Program.jsx
@@ -13,11 +13,13 @@ const Program = () => {
             Fredag - Knarrlagsundet
           </h2>
           <p>
-            18:00	Middag på Knarren 
+            18:00	Middag på Knarren*
             <br/>
-            20:00	Lek og hygge i Knarrlaget 
+            20:00	Lek og hygge i Knarrlaget*
             <br/>
             00:00	Ro i Knarrlaget
+            <br/>
+            Om du ønsker å ta noe å drikke når vi trekker oss tilbake til Knarrlaget kl 20:00, ta tuen innom Knarrlaget før middagen på Knarren og legg fra deg drikken.
           </p>
         </div>
         <div>
